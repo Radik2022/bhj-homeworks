e0d38e641e28c5bac144e4a7cba58e674530768c
Radik2022 <109106491+Radik2022@users.noreply.github.com>
2023-06-21 20:34:52 +0300

    Update task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,13 @@
+const dropdownList = document.querySelector('.dropdown__list');
+const dropdownValue = document.querySelector('.dropdown__value');
+const dropdownItem = Array.from(document.querySelectorAll('.dropdown__item'));
+
+function onclickValue() { dropdownList.classList.toggle('dropdown__list_active')};
+dropdownValue.addEventListener('click', onclickValue);
+
+function onclickItem(event) {
+    dropdownValue.textContent = event.target.textContent;
+    dropdownList.classList.remove('dropdown__list_active')
+    return false;
+};
+dropdownItem.forEach(element => element.onclick = onclickItem);
