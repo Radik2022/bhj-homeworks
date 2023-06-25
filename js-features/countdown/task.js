39602ba460b7ea65e42dca8edfe3c539d51687bb
Radik2022 <109106491+Radik2022@users.noreply.github.com>
2023-06-25 19:23:34 +0300

    исправлен task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,29 +1,12 @@
-const deadline = new Date(2023, 04, 04, 13, 10, 10);
-const timerSeconds = document.querySelector('#timerSeconds');
-const timerHours = document.querySelector('#timerHours');
-const timerMinutes = document.querySelector('#timerMinutes');
-const timerDays = document.querySelector('#timerDays');
-const idInterval = setInterval(countingDownTheTime, 1000);
+let timer = document.getElementById("timer");
 
-
-
-function countingDownTheTime() {
-    const diff = deadline - new Date();
-
-    if(diff <= 0) {
+function timerMinus() {
+    if (timer.textContent <= 0) {
         clearInterval(idInterval);
-       alert('Вы победили в конкурсе!')
-       
+        alert("Вы победили в конкурсе!");
     } else {
-        const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
-        const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
-        const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
-        const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
-        timerDays.textContent = days < 10 ? '0' + days : days;
-        timerHours.textContent = hours < 10 ? '0' + hours : hours;
-        timerMinutes.textContent = minutes < 10 ? '0' + minutes : minutes;
-        timerSeconds.textContent = seconds < 10 ? '0' + seconds : seconds;
-        
+        timer.textContent--;
     }
+};
 
-}
+let idInterval = setInterval(timerMinus, 1000);
