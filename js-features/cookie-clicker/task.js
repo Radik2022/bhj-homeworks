a026f539e5333ba4c847f26ab79d79aecf71fa3c
Radik2022 <109106491+Radik2022@users.noreply.github.com>
2023-06-25 19:29:27 +0300

    исправлен task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,23 +1,13 @@
-const clicker__counter = document.querySelector('#clicker__counter');
-const cookie = document.querySelector('#cookie');
-const click__speed = document.querySelector('#click__speed');
-let newDate = null;
+let clickerCounter = document.getElementById("clicker__counter");
+let cookie = document.getElementById("cookie");
+let counter = 0;
 
-cookie.onclick = function () {
-    let timer = Number(clicker__counter.textContent);
-    if(timer === 0){
-        newDate =  new Date();
-    }
-
-    if(timer >= 0 && timer < 666){
-        clicker__counter.textContent = timer + 1;
-        cookie.classList.toggle('cookieAdd');
-        let now = new Date();
-        let result = (now - newDate) / 100 ;
-        click__speed.textContent = result.toFixed(1);
-        newDate = new Date();
+cookie.onclick = function() {
+  counter++;
+  clickerCounter.textContent = counter;
+  if (counter % 2 == 0) {
+    cookie.width = 200;                  
     } else {
-        clicker__counter.textContent = 0;
+      cookie.width = 180;
     }
-
-};
+  }
