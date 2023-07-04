8f8cda6283b38f81a7cf12f79ae6c7e7383ce2a0
Radik2022 <109106491+Radik2022@users.noreply.github.com>
2023-07-04 20:15:50 +0300

    исправлено task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,48 +1,24 @@
-const tooltips = Array.from(document.getElementsByClassName('has-tooltip')); 
-let tooltip = document.createElement('div'); 
-tooltip.className = 'tooltip'; 
-tooltip.innerHTML = 'Tooltip'; 
+const hasTooltip = Array.from(document.querySelectorAll('.has-tooltip'));
 
- 
-function setPositionTooltip(elem) { 
-    let tooltipWidth = tooltip.offsetWidth; 
-    let tooltipHeight = tooltip.offsetHeight; 
-    let elemRect = elem.getBoundingClientRect(); 
-    let left = elemRect.left + (elem.offsetWidth / 2) - (tooltipWidth / 2); 
-    let top = elemRect.top - tooltipHeight - 5; 
-
-    if (left < 0) left = 0;  
-    if (left + tooltipWidth > document.documentElement.clientWidth) left = document.documentElement.clientWidth - tooltipWidth;  
-     
-    tooltip.style.left = left + 'px'; 
-    tooltip.style.top = top + 'px'; 
-} 
-
-tooltips.forEach(elem => { 
-    elem.addEventListener('click', event => { 
-        event.preventDefault(); 
-        let target = event.target; 
-
-        if (target.title === tooltip.innerText) { 
-            tooltip.classList.toggle('tooltip_active'); 
-            return; 
-        } 
-
-        tooltip.innerText = target.title; 
-        target.insertAdjacentElement('afterEnd', tooltip); 
-        setPositionTooltip(elem); 
-        tooltip.classList.add('tooltip_active'); 
-    }); 
-}); 
-
-document.addEventListener('mousemove', event => { 
-    if (tooltip.classList.contains('tooltip_active')) { 
-        setPositionTooltip(tooltip.previousSibling); 
-    } 
-}); 
-
-document.addEventListener('scroll', event => { 
-    if (tooltip.classList.contains('tooltip_active')) { 
-        setPositionTooltip(tooltip.previousSibling); 
-    } 
-});
+hasTooltip.forEach(el => {
+	const tooltip = document.createElement('div');
+	tooltip.textContent = el.title;
+	el.addEventListener('click', (event) => {
+		event.preventDefault();
+		tooltip.classList.add('tooltip');
+		el.insertAdjacentElement('beforeBegin', tooltip);
+		tooltip.style.left = `${el.getBoundingClientRect().left}px`;
+		tooltip.style.top = `${el.getBoundingClientRect().bottom}px`;
+		const activeTooltip = Array.from(document.querySelectorAll('.tooltip_active'));
+		const activeFind = activeTooltip.find(elem => elem.classList.contains('tooltip_active'));
+		if (activeFind === undefined) {
+			tooltip.classList.add('tooltip_active');
+		} else {
+			if (activeFind !== tooltip) {
+				tooltip.classList.add("tooltip_active");
+			}
+			activeFind.classList.toggle('tooltip_active');
+		}
+	}
+	)
+})
