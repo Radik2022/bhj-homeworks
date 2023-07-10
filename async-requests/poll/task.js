f43d988aaa34884fcb002af25861859484be64bc
Radik2022 <109106491+Radik2022@users.noreply.github.com>
2023-07-10 19:48:54 +0300

    исправлен task.js

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,50 +1,20 @@
 const pollTitle = document.getElementById("poll__title");
-const pollAnswer = document.getElementById("poll__answers");
+const pollAnswers = document.getElementById("poll__answers");
 
-const xml = new XMLHttpRequest();
-xml.open("GET", "https://students.netoservices.ru/nestjs-backend/poll");
-xml.send();
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/poll");
+xhr.send();
 
-let dataPost;
-
-xml.addEventListener("readystatechange", () => {
-    if (xml.readyState === xml.DONE && xml.statusText === "OK") {
-        let data = JSON.parse(xml.response);
-
-        pollTitle.textContent = data.data.title;
-
-        data.data["answers"].forEach((element) => {
-            pollAnswer.insertAdjacentHTML(
-                "afterbegin",
-                `
-        <button class="poll__answer">
-          ${element} 
-        </button>`
-            );
-        });
-
-        const btn = Array.from(document.querySelectorAll(".poll__answer"));
-
-        btn.forEach((element, index) => {
-            element.addEventListener("click", () => {
-                alert("Спасибо, ваш голос засчитан!");
-
-                const xhr = new XMLHttpRequest();
-                xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/poll");
-                xhr.setRequestHeader(
-                    "Content-type",
-                    "application/x-www-form-urlencoded"
-                );
-                xhr.send(`vote=${data.id}&answer=${index}`);
-                xhr.addEventListener("readystatechange", () => {
-                    if (xhr.readyState === xhr.DONE && xhr.statusText === "OK") {
-                        dataPost = JSON.parse(xhr.response);
-                        pollAnswer.textContent = "";
-
-                    }
-                    setTimeout(() => window.location.reload(), 9000);
-                });
-            });
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        const response = JSON.parse(xhr.responseText);
+        pollTitle.innerText = response.data.title;
+        response.data.answers.forEach(answer => {
+            const button = document.createElement('button');
+            button.classList.add("poll__answer");
+            button.innerText = `${answer}`;
+            button.addEventListener("click", function () {alert("Спасибо, ваш голос засчитан!")});
+            pollAnswers.appendChild(button);
         });
     }
-});
+};
